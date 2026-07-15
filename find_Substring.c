c48857944686fc3833fc0c9adabfbe02f30b42dc
Hardik Chilana <hardik1222.becse24@chitkara.edu.in>
2026-07-15 20:47:41 +0530

    Implement substring search function in C

---
+++ find_Substring.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int find_substring(const char *haystack, const char *needle) {
+    int i, j;
+
+    // If needle is empty, return 0
+    if (needle[0] == '\0')
+        return 0;
+
+    for (i = 0; haystack[i] != '\0'; i++) {
+        j = 0;
+
+        while (needle[j] != '\0' &&
+               haystack[i + j] != '\0' &&
+               haystack[i + j] == needle[j]) {
+            j++;
+        }
+
+        if (needle[j] == '\0') {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    const char *haystack = "Embedded Systems";
+    const char *needle = "bed";
+
+    int index = find_substring(haystack, needle);
+
+    if (index != -1)
+        printf("Substring found at index: %d\n", index);
+    else
+        printf("Substring not found\n");
+
+    return 0;
+}
